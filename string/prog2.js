12bc70e6e551d19964795f11ba05078423514456
Shabin-Hussain <shabinhussain112@gmail.com>
2026-07-07 20:45:49 +0530

    new

--- string/prog2.js
+++ string/prog2.js
@@ -5,3 +5,16 @@ console.log(str.trim());
 
 str1 = "      dsfdsfasf"
 console.log(str1.trim());
+
+//substring()
+
+str= 'luminar'
+str2 = str.substring(0,4)
+console.log(str2);
+
+str3=str.substring(2)
+console.log(str3);
+
+str4=str.substring(2,5)
+console.log(str4);
+
---
+++ string/prog3.js
@@ -0,0 +1,14 @@
+//w.a.p to check whether the given number is mobile number or not(10 digit)
+
+num = '876543789'
+console.log(num.length==10?'its a mobile number':'not a mobile number');
+
+//w.a.p to check the given mail id is gmail.com(@gmail.com)
+id = 'raj@gmai.com'
+console.log(id.endsWith('@gmail.com')?'yes':'no');
+
+//w.a.p to check a word starts with the letter q
+ word = 'ueen'
+ console.log( word.startsWith('q')|| word.startsWith('Q')?'yes':'no');
+ 
+
