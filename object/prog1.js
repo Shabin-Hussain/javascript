7929bf6fc277dffd2bf2d341b9dfd603b66225b6
Shabin-Hussain <shabinhussain112@gmail.com>
2026-07-08 15:31:41 +0530

    new notes

--- object/prog1.js
+++ object/prog1.js
@@ -8,3 +8,45 @@ employeeDetails={
 }
 
 console.log(employeeDetails);
+
+//display the employee name
+
+console.log(employeeDetails.name);
+
+//or
+
+console.log(employeeDetails["name"]);
+
+
+for(let key in employeeDetails){
+    console.log(key);
+    
+}
+
+//w.a.p to print yes if gender key is present else print no
+
+if('gender' in employeeDetails){
+   console.log('present');
+    
+    
+}
+else{
+    console.log('not present');
+    
+}
+
+console.log('gender' in employeeDetails?'present':employeeDetails["gender"]='male');
+
+//how to add a key and value
+//Objectname["key"]=value
+
+/* employeeDetails["gender"]='male' */
+console.log(employeeDetails);
+
+//update
+employeeDetails.exp=5
+console.log(employeeDetails);
+
+
+
+
