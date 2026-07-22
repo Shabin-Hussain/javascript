edde01a4f1c58c5bd47056b2782c7f2541340e7c
Shabin-Hussain <shabinhussain112@gmail.com>
2026-07-22 18:31:30 +0530

    updated program

---
+++ Javascript Frontend/todo/todo.js
@@ -0,0 +1,19 @@
+
+
+ function add(){
+    /*      console.log(act.value);  */
+
+     if(act.value){
+        result.innerHTML +=`<li>${act.value}</li>`
+
+     }
+     else{
+        console.log('Enter any value');
+        
+     } 
+        
+    }
+
+
+
+add()
